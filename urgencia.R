d70a6fe68a95e3f02f44d67fc349d4fdbe6d9968
mgaldino <mcz.fea@gmail.com>
2024-02-22 18:37:20 -0300

    trabalho feito

--- urgencia.R
+++ urgencia.R
@@ -2,7 +2,7 @@
 library(httr)
 library(jsonlite)
 library(tidyverse)
-
+library(stringr)
 
 ##
 
@@ -43,7 +43,7 @@ get_proposicoes <- function(year) {
   return(proposicoes)
 }
 
-vec_ano <- 2017:2023
+vec_ano <- 2010:2023
 lista_proposicoes <- list()
 lista_urgencia <- list()
 for (i in 1:length(vec_ano)){
@@ -55,7 +55,51 @@ for (i in 1:length(vec_ano)){
 }
 
 proposicoes <- bind_rows(lista_proposicoes)
-urgencia <- bind_rows(lista_urgencia)
+
+urgencia <- bind_rows(lista_urgencia) 
+
+# dput(c(urgencia$ementa[urgencia$id==469773], urgencia$ementa[urgencia$id==469774], urgencia$ementa[urgencia$id==469956]))
+
+
+urgencia <- urgencia %>%
+  mutate(ementa_simplificada = gsub("(°|º)", "", ementa ),
+         ementa_simplificada = tolower(ementa_simplificada),
+         ementa_simplificada = str_squish(ementa_simplificada),
+         ementa_simplificada = gsub("\\\\", "/", ementa_simplificada))# Substituir barras invertidas por barras regulares
+
+
+# Expressão regular ajustada para capturar o ano em diferentes formatos
+padrao <- "(projeto de decreto legislativo|projeto de decreto legislativo (pdc)|prc|mensagem|pdc|plp|pl|pdl|msc|projeto de lei|projeto de lei (pl)|projeto de lei do senado federal|projeto de lei complementar|projeto de resolução)\\s?(n|no)?\\.?\\s?([0-9]+[,.\\s]?[0-9]*[A-Za-z]?)(,? de |/|/ )?(\\d{2,4})"
+
+# Extrair as informações usando str_extract
+# resultados <- str_extract(urgencia$ementa_simplificada, padrao)
+
+# grepl("apreciação de proposição", "requer regime de urgência para apreciação de proposição 736 de 2010..")
+
+# urgencia$ementa[75]
+
+urgencia <- urgencia %>%
+  mutate(apreciacao = str_extract(ementa_simplificada, padrao),
+         apreciacao = ifelse(!is.na(apreciacao), apreciacao,
+                             ifelse(grepl("apreciação d[ae] proposição", ementa_simplificada), "proposicao sem mais", siglaTipo)),
+         e_urgencia = grepl("urgência", ementa_simplificada),
+         e_projeto_resolução = grepl("projeto de resolução", ementa_simplificada))
+
+urgencia <- urgencia %>%
+  mutate(apreciacao2 = ifelse(e_urgencia & !e_projeto_resolução & apreciacao == "REQ" & grepl("projeto de [0-9]+", ementa_simplificada),
+                str_extract(ementa_simplificada, "projeto de [0-9]+/[0-9]+"), "erro"))
+
+
+tipo_pl_urgencia <- urgencia %>%
+  inner_join(select(proposicoes, c(id, siglaTipo, numero, ano)), by="id") %>%
+  arrange(id)
+
+
+grepl("projeto de [0-9]+","solicita urgência para apreciação do projeto de 1669/2009")
+str_extract("solicita urgência para apreciação do projeto de 1669/2009", "projeto de [0-9]+")
+
+unique(urgencia$siglaTipo)
+unique(urgencia$codTipo)
 
 urgencia_total_ano <- urgencia %>%
   group_by(ano) %>%
@@ -78,7 +122,7 @@ write.table(df_final, file = "urgencia_17_23.csv", sep=";", row.names = FALSE)
 
 # apenas PL
 pl_total_ano <- proposicoes %>%
-  filter(siglaTipo == "PL") %>%
+  filter(siglaTipo %in% c("PL", "PLP", "PLC", "PLD", "PLN", "PDL", "PLS") %>%
   group_by(ano) %>%
   summarise(num_prop = n_distinct(id))
 
